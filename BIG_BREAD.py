88c180e3e6ae1f32062cb6a67ad466c564346dfd
Juan José Arato <131078432+juanjosearato@users.noreply.github.com>
2023-05-22 16:44:08 -0300

    Add files via upload

---
+++ BIG_BREAD.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+class Conectar_Base_de_Datos():
+    def __init__(self) -> None:
+        try:
+            self.conexion = mysql.connector.connect(
+                host = 'localhost',
+                port = 3306,
+                user = 'root',
+                password = '*****',
+                db = 'bd_BIG_BREAD'
+            )
+        except mysql.connector.Error as descripcionError:
+            print("¡No se conectó la base de datos!',descripcionError")
+            
