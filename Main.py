48af7505dea6b045f384760ddb9bdc8ea2622628
Romic361 <111034854+Romic361@users.noreply.github.com>
2023-06-11 15:23:20 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,11 +15,11 @@ if ingresaProducto == Y:
     tipoProducto = varchar(input("Ingrese si contiene tacc o no el producto:")
     incluyePromoProducto = vachar(input("Ingrese si el producto incluye promo:")
 
-    producto = Producto(0,nombreProducto,descripcionProducto,precioProducto,tipoProducto,0)
+    producto = Producto(0,nombreProducto,descripcionProducto,precioProducto,tipoProducto,incluyePromoProducto,0)
     conexionBD.Insertar_Producto(producto)
     
-elif  ingresaProducto == 2:
+elif  ingresaProducto == L:
     con = conexionBD.Conectar()
     prod=conexionBD.Listado_De_Productos()
     for i in prod:
-        print("Nombre:" + i[1] + "Stock:" + i[2] + "Precio" + i[3]+ "Unidad de medida del producto" + i[4])
+        print("Nombre:" + i[1] + "descripcion:" + i[2] + "Precio" + i[3]+ "tipo de producto" + i[4] + "incluye promo:" + i[5])
